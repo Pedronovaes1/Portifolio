12eb13e858d0455006762f9d891024709d72cd87
Pedronovaes1 <pedro.novaes@souunit.com.br>
2025-01-23 20:39:37 -0300

    melhorando o portifólio

---
+++ script.js
@@ -0,0 +1,15 @@
+function openModal() {
+    document.getElementById('modal').classList.add('show');
+}
+
+function closeModal() {
+    document.getElementById('modal').classList.remove('show');
+}
+
+function openModal2() {
+    document.getElementById('modal2').classList.add('show');
+}
+
+function closeModal2() {
+    document.getElementById('modal2').classList.remove('show');
+}
